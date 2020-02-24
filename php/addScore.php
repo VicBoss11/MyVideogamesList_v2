d04bf29d9ba371e82615a35b2de6949a162feec4
Victor <victor.af1104@gmail.com>
2020-02-24 01:01:06 +0100

    Condiciones para las notas de narrativa y multijugador establecidas

--- php/addScore.php
+++ php/addScore.php
@@ -8,17 +8,21 @@ if ($mysqli_connection->connect_errno) {
 }
 
 $videogame_id = $_GET["videogame_id"];
-$scores[] = $_GET["gameplay_score"];
-$scores[] = $_GET["graphics_score"];
-$scores[] = $_GET["art_score"];
-$scores[] = $_GET["sound_score"];
+$scores[0] = $_GET["gameplay_score"];
+$scores[1] = $_GET["graphics_score"];
+$scores[2] = $_GET["art_score"];
+$scores[3] = $_GET["sound_score"];
+$narrative = NULL;
+$multiplayer = NULL;
 
-if (isset($_GET["narrative_score"])) {
-    $scores[] = $_GET["narrative_score"];
+if ($_GET["narrative_score"] != FALSE) {
+    $scores[4] = $_GET["narrative_score"];
+    $narrative = $scores[4];
 }
 
-if (isset($_GET["multiplayer_score"])) {
-    $scores[] = $_GET["multiplayer_score"];
+if ($_GET["multiplayer_score"] != FALSE) {
+    $scores[5] = $_GET["multiplayer_score"];
+    $multiplayer = $scores[5];
 }
 
 $count = 0;
@@ -34,7 +38,7 @@ $score = round($avg_scores * 10);
 $stmt = $mysqli_connection->stmt_init();
 
 $stmt->prepare("INSERT INTO sections-score VALUES (?, ?, ?, ?, ?, ?, ?)");
-$stmt->bind_param("sffffff", $id, $scores[0], $scores[1], $scores[2], $scores[3], $scores[4], $scores[5]);
+$stmt->bind_param("sffffff", $id, $scores[0], $scores[1], $scores[2], $scores[3], $narrative, $multiplayer);
 
 if (!$stmt->execute()) {
     echo "Error al ejecutar la consulta: (" . $stmt->errno . ") " . $stmt->error;
